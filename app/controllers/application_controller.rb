c0ba61c9f670042deacefa5177bad233ae2001d3
strawberryisamu <kiuchi-isamu@life-coach.co.jp>
2026-03-08 05:49:02 +0900

    Initialize repository

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    render html:"¡Hola, mundo!"
+  end
+
+  def goodbye
+    render html:"¡Adiós, mundo!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  root "application#hello"
+  get "/goodbye", to: "application#goodbye"
 end
