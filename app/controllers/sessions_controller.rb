993426b0f4adc541e09a6e7a2ca0dbc5db58a49c
strawberryisamu <kiuchi-isamu@life-coach.co.jp>
2026-03-09 10:27:38 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      reset_session      # ログインの直前に必ずこれを書くこと
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,10 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+  
   test "should be valid" do
     assert @user.valid?
   end
@@ -62,5 +66,5 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
-  
+
 end
