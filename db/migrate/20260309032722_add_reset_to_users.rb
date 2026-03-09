cf204ae1dbea07dd5301735845def527ef03642b
strawberryisamu <kiuchi-isamu@life-coach.co.jp>
2026-03-09 13:43:23 +0900

    update sh file

--- db/migrate/20260309032722_add_reset_to_users.rb
+++ db/migrate/20260309032722_add_reset_to_users.rb
@@ -1,4 +1,4 @@
-class AddResetToUsers < ActiveRecord::Migration[7.0]
+bin/rails g migration AddResetSentAtToUsers reset_sent_at:datetimeclass AddResetToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :reset_digest, :string
   end
